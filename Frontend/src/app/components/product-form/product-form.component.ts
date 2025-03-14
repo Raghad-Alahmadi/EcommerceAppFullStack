f612b11cd08dc40b8fa238b5bca48a8a1d84708b
Raghad-Alahmadi <tu4152758@taibahu.edu.sa>
2025-03-14 16:07:59 +0300

    refactor product rating structure and update related components

--- Frontend/src/app/components/product-form/product-form.component.ts
+++ Frontend/src/app/components/product-form/product-form.component.ts
@@ -11,7 +11,6 @@ import * as ProductActions from '../../store/product.actions';
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './product-form.component.html',
   styleUrls: ['./product-form.component.css'],
-  
 })
 export class ProductFormComponent {
   productForm: FormGroup;
@@ -23,24 +22,23 @@ export class ProductFormComponent {
       description: ['', Validators.required],
       category: ['', Validators.required],
       image: ['', Validators.required],
-      rating: this.fb.group({
-        rate: ['', [Validators.required, Validators.min(0), Validators.max(5)]],
-        count: ['', [Validators.required, Validators.min(0)]]
-      })
+      rate: ['', [Validators.required, Validators.min(0), Validators.max(5)]],
+      count: ['', [Validators.required, Validators.min(0)]]
     });
   }
-showForm = false;
 
-toggleForm(): void {
-  this.showForm = !this.showForm;
-}
+  showForm = false;
+
+  toggleForm(): void {
+    this.showForm = !this.showForm;
+  }
+
   onSubmit(): void {
     if (this.productForm.valid) {
       const product: Product = this.productForm.value;
       this.store.dispatch(ProductActions.addProduct({ product }));
       this.productForm.reset(); // Reset the form after submission
     } else {
-      // Mark all controls as touched to trigger validation messages
       this.productForm.markAllAsTouched();
       console.log('Form is invalid');
     }
