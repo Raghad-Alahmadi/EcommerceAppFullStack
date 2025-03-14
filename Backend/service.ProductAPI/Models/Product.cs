6b7577f6ff6c129f96725c62fd01b5a51c6db466
Raghad-Alahmadi <tu4152758@taibahu.edu.sa>
2025-03-14 16:00:14 +0300

    set up backend API with initial configuration and product service integration

---
+++ Backend/service.ProductAPI/Models/Product.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace service.ProductAPI.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        [Url]
+        public string Image { get; set; }
+
+
+        [Range(0.0, 5.0)]
+        public double Rate { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int Count { get; set; }
+    }
+
+}
